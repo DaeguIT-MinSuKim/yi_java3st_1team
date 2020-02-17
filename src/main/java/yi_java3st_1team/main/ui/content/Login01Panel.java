ad2e0461c7ef9373be4d7f9c941b60aef5c2c35b
jamesareumin <hothihi5@gmail.com>
2020-02-17 21:26:51 +0900

    변수명 수정

--- src/main/java/yi_java3st_1team/main/ui/content/Login01Panel.java
+++ src/main/java/yi_java3st_1team/main/ui/content/Login01Panel.java
@@ -1,17 +1,19 @@
 package yi_java3st_1team.main.ui.content;
 
-import javax.swing.JPanel;
 import java.awt.Dimension;
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
 import java.awt.Font;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import javax.swing.ImageIcon;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+@SuppressWarnings("serial")
 public class Login01Panel extends JPanel {
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField tfId;
+	private JTextField tfPass;
 
 	/**
 	 * Create the panel.
@@ -24,57 +26,57 @@ public class Login01Panel extends JPanel {
 		setPreferredSize(new Dimension(350, 348));
 		setLayout(null);
 		
-		JLabel label = new JLabel("");
-		label.setIcon(new ImageIcon("D:\\workspace\\workspace_gradle\\yi_java3st_1team\\images\\loginMain\\user.png"));
-		label.setHorizontalAlignment(SwingConstants.CENTER);
-		label.setBounds(20, 65, 41, 41);
-		add(label);
+		JLabel lblImg01 = new JLabel("");
+		lblImg01.setIcon(new ImageIcon("D:\\workspace\\workspace_gradle\\yi_java3st_1team\\images\\loginMain\\user.png"));
+		lblImg01.setHorizontalAlignment(SwingConstants.CENTER);
+		lblImg01.setBounds(20, 65, 41, 41);
+		add(lblImg01);
 		
-		JLabel label_1 = new JLabel("Username");
-		label_1.setFont(new Font("Arial", Font.BOLD, 20));
-		label_1.setBounds(75, 66, 97, 41);
-		add(label_1);
+		JLabel lblId = new JLabel("Username");
+		lblId.setFont(new Font("Arial", Font.BOLD, 20));
+		lblId.setBounds(75, 66, 97, 41);
+		add(lblId);
 		
-		textField = new JTextField();
-		textField.setFont(new Font("굴림", Font.PLAIN, 17));
-		textField.setColumns(10);
-		textField.setBounds(180, 60, 151, 48);
-		add(textField);
+		tfId = new JTextField();
+		tfId.setFont(new Font("굴림", Font.PLAIN, 17));
+		tfId.setColumns(10);
+		tfId.setBounds(180, 60, 151, 48);
+		add(tfId);
 		
-		JLabel label_2 = new JLabel("");
-		label_2.setIcon(new ImageIcon("D:\\workspace\\workspace_gradle\\yi_java3st_1team\\images\\loginMain\\lock.png"));
-		label_2.setHorizontalAlignment(SwingConstants.CENTER);
-		label_2.setFont(new Font("굴림", Font.PLAIN, 20));
-		label_2.setBounds(20, 160, 41, 41);
-		add(label_2);
+		JLabel lblImg02 = new JLabel("");
+		lblImg02.setIcon(new ImageIcon("D:\\workspace\\workspace_gradle\\yi_java3st_1team\\images\\loginMain\\lock.png"));
+		lblImg02.setHorizontalAlignment(SwingConstants.CENTER);
+		lblImg02.setFont(new Font("굴림", Font.PLAIN, 20));
+		lblImg02.setBounds(20, 160, 41, 41);
+		add(lblImg02);
 		
-		JLabel label_3 = new JLabel("Password");
-		label_3.setFont(new Font("Arial", Font.BOLD, 20));
-		label_3.setBounds(75, 167, 97, 31);
-		add(label_3);
+		JLabel lblPass = new JLabel("Password");
+		lblPass.setFont(new Font("Arial", Font.BOLD, 20));
+		lblPass.setBounds(75, 167, 97, 31);
+		add(lblPass);
 		
-		textField_1 = new JTextField();
-		textField_1.setFont(new Font("굴림", Font.PLAIN, 17));
-		textField_1.setColumns(10);
-		textField_1.setBounds(180, 160, 151, 48);
-		add(textField_1);
+		tfPass = new JTextField();
+		tfPass.setFont(new Font("굴림", Font.PLAIN, 17));
+		tfPass.setColumns(10);
+		tfPass.setBounds(180, 160, 151, 48);
+		add(tfPass);
 		
-		JButton button = new JButton("LOGIN");
-		button.setFont(new Font("Arial", Font.BOLD, 17));
-		button.setFocusable(false);
-		button.setBounds(20, 245, 120, 45);
-		add(button);
+		JButton btnLogin = new JButton("LOGIN");
+		btnLogin.setFont(new Font("Arial", Font.BOLD, 17));
+		btnLogin.setFocusable(false);
+		btnLogin.setBounds(20, 245, 120, 45);
+		add(btnLogin);
 		
-		JButton button_1 = new JButton("REGISTER");
-		button_1.setFont(new Font("Arial", Font.BOLD, 17));
-		button_1.setFocusable(false);
-		button_1.setBounds(147, 245, 120, 45);
-		add(button_1);
+		JButton btnResiter = new JButton("REGISTER");
+		btnResiter.setFont(new Font("Arial", Font.BOLD, 17));
+		btnResiter.setFocusable(false);
+		btnResiter.setBounds(147, 245, 120, 45);
+		add(btnResiter);
 		
-		JButton button_2 = new JButton("");
-		button_2.setIcon(new ImageIcon("D:\\workspace\\workspace_gradle\\yi_java3st_1team\\images\\loginMain\\searchBtn.png"));
-		button_2.setFocusable(false);
-		button_2.setBounds(273, 245, 55, 45);
-		add(button_2);
+		JButton btnSearch = new JButton("");
+		btnSearch.setIcon(new ImageIcon("D:\\workspace\\workspace_gradle\\yi_java3st_1team\\images\\loginMain\\searchBtn.png"));
+		btnSearch.setFocusable(false);
+		btnSearch.setBounds(273, 245, 55, 45);
+		add(btnSearch);
 	}
 }
