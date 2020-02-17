fbe639da1b603b234d8df29db6aed3cc0df0d8e1
jamesareumin <hothihi5@gmail.com>
2020-02-17 20:36:41 +0900

    로그인후 패널 작성

--- src/main/java/yi_java3st_1team/main/ui/content/Login02Panel.java
+++ src/main/java/yi_java3st_1team/main/ui/content/Login02Panel.java
@@ -10,8 +10,6 @@ import javax.swing.JButton;
 import javax.swing.ImageIcon;
 
 public class Login02Panel extends JPanel {
-	private JTextField textField;
-	private JTextField textField_1;
 
 	/**
 	 * Create the panel.
@@ -24,57 +22,30 @@ public class Login02Panel extends JPanel {
 		setPreferredSize(new Dimension(350, 348));
 		setLayout(null);
 		
-		JLabel label = new JLabel("");
-		label.setIcon(new ImageIcon("D:\\workspace\\workspace_gradle\\yi_java3st_1team\\images\\loginMain\\user.png"));
+		JButton btnLogout = new JButton("LOGOUT");
+		btnLogout.setFont(new Font("Arial", Font.BOLD, 17));
+		btnLogout.setFocusable(false);
+		btnLogout.setBounds(20, 245, 120, 45);
+		add(btnLogout);
+		
+		JLabel label = new JLabel("[관리자 로그인]");
 		label.setHorizontalAlignment(SwingConstants.CENTER);
-		label.setBounds(20, 65, 41, 41);
+		label.setFont(new Font("휴먼둥근헤드라인", Font.PLAIN, 18));
+		label.setBounds(149, 59, 174, 45);
 		add(label);
 		
-		JLabel label_1 = new JLabel("Username");
-		label_1.setFont(new Font("Arial", Font.BOLD, 20));
-		label_1.setBounds(75, 66, 97, 41);
+		JLabel label_1 = new JLabel("<html>반갑습니다!<br><span style='color:blue'>권수진 인턴</span>님이<br>로그인 하셨습니다.</html>");
+		label_1.setFont(new Font("굴림", Font.BOLD, 17));
+		label_1.setBounds(149, 101, 174, 91);
 		add(label_1);
 		
-		textField = new JTextField();
-		textField.setFont(new Font("굴림", Font.PLAIN, 17));
-		textField.setColumns(10);
-		textField.setBounds(180, 60, 151, 48);
-		add(textField);
-		
-		JLabel label_2 = new JLabel("");
-		label_2.setIcon(new ImageIcon("D:\\workspace\\workspace_gradle\\yi_java3st_1team\\images\\loginMain\\lock.png"));
-		label_2.setHorizontalAlignment(SwingConstants.CENTER);
-		label_2.setFont(new Font("굴림", Font.PLAIN, 20));
-		label_2.setBounds(20, 160, 41, 41);
+		JLabel label_2 = new JLabel("<html>- 사원번호 : <span style='color:red'>ET123</span><br>- <span style='color:green'>상품관리부</span></html>");
+		label_2.setFont(new Font("굴림", Font.BOLD, 17));
+		label_2.setBounds(149, 233, 174, 45);
 		add(label_2);
 		
-		JLabel label_3 = new JLabel("Password");
-		label_3.setFont(new Font("Arial", Font.BOLD, 20));
-		label_3.setBounds(75, 167, 97, 31);
+		JLabel label_3 = new JLabel("이미지삽입");
+		label_3.setBounds(20, 59, 120, 120);
 		add(label_3);
-		
-		textField_1 = new JTextField();
-		textField_1.setFont(new Font("굴림", Font.PLAIN, 17));
-		textField_1.setColumns(10);
-		textField_1.setBounds(180, 160, 151, 48);
-		add(textField_1);
-		
-		JButton button = new JButton("LOGIN");
-		button.setFont(new Font("Arial", Font.BOLD, 17));
-		button.setFocusable(false);
-		button.setBounds(20, 245, 120, 45);
-		add(button);
-		
-		JButton button_1 = new JButton("REGISTER");
-		button_1.setFont(new Font("Arial", Font.BOLD, 17));
-		button_1.setFocusable(false);
-		button_1.setBounds(147, 245, 120, 45);
-		add(button_1);
-		
-		JButton button_2 = new JButton("");
-		button_2.setIcon(new ImageIcon("D:\\workspace\\workspace_gradle\\yi_java3st_1team\\images\\loginMain\\searchBtn.png"));
-		button_2.setFocusable(false);
-		button_2.setBounds(273, 245, 55, 45);
-		add(button_2);
 	}
 }
