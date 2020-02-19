8523e752c230a653c963bcf12ec9d012c4a733bb
jamesareumin <hothihi5@gmail.com>
2020-02-19 14:29:59 +0900

    미니 차트 파일 시작

---
+++ src/main/java/yi_java3st_1team/main/ui/content/chart/ClientMiniChartPanel.java
@@ -0,0 +1,32 @@
+package yi_java3st_1team.main.ui.content.chart;
+
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+
+public class ClientMiniChartPanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public ClientMiniChartPanel() {
+
+		initialize();
+	}
+	private void initialize() {
+		setPreferredSize(new Dimension(350, 250));
+		setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblLogo = new JLabel("");
+		lblLogo.setOpaque(true);
+		lblLogo.setBackground(Color.WHITE);
+		lblLogo.setHorizontalAlignment(SwingConstants.CENTER);
+		lblLogo.setIcon(new ImageIcon("D:\\workspace\\workspace_gradle\\yi_java3st_1team\\images\\loginMain\\teamLog.png"));
+		add(lblLogo, BorderLayout.CENTER);
+	}
+
+}
---
+++ src/main/java/yi_java3st_1team/main/ui/content/chart/EmpMiniChart02Panel.java
@@ -0,0 +1,38 @@
+package yi_java3st_1team.main.ui.content.chart;
+
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.awt.SystemColor;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+
+public class EmpMiniChart02Panel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public EmpMiniChart02Panel() {
+
+		initialize();
+	}
+	private void initialize() {
+		setBackground(SystemColor.inactiveCaptionBorder);
+		setPreferredSize(new Dimension(1134, 348));
+		setLayout(null);
+		
+		JLabel lblImg1 = new JLabel("");
+		lblImg1.setIcon(new ImageIcon("D:\\workspace\\workspace_gradle\\yi_java3st_1team\\images\\loginMain\\mainLogo1.png"));
+		lblImg1.setBounds(149, 37, 286, 169);
+		add(lblImg1);
+		
+		JLabel lblImg2 = new JLabel("");
+		lblImg2.setIcon(new ImageIcon("D:\\workspace\\workspace_gradle\\yi_java3st_1team\\images\\loginMain\\mainLogo3.png"));
+		lblImg2.setBounds(669, 37, 335, 298);
+		add(lblImg2);
+		
+		JLabel lblImg3 = new JLabel("");
+		lblImg3.setIcon(new ImageIcon("D:\\workspace\\workspace_gradle\\yi_java3st_1team\\images\\loginMain\\mainLogo2.png"));
+		lblImg3.setBounds(159, 216, 325, 119);
+		add(lblImg3);
+	}
+}
