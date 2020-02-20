5ab648e422f9f81b5ab9f1b258166174e32852bb
ksj-alt <ham_a127@naver.com>
2020-02-20 14:37:02 -0500

    주문 확인 및 처리 / 주문현황 패널 수정, 고객 주문등록 메인 패널 위치 변경

--- src/main/java/yi_java3st_1team/ordermanagement/ui/ClientOMainPanel.java
+++ src/main/java/yi_java3st_1team/ordermanagement/ui/ClientOMainPanel.java
@@ -1,4 +1,4 @@
-package yi_java3st_1team.ordermanagement.ui.content;
+package yi_java3st_1team.ordermanagement.ui;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
@@ -11,20 +11,21 @@ import javax.swing.JTabbedPane;
 import yi_java3st_1team.ordermanagement.ui.list.ClientOListPanel;
 import yi_java3st_1team.ordermanagement.ui.list.OListPanel;
 import yi_java3st_1team.ordermanagement.ui.panel.ClientORegisterPanel;
-import yi_java3st_1team.ordermanagement.ui.panel.OCheckPanel;
+import yi_java3st_1team.ordermanagement.ui.panel.ClientOCheckPanel;
 import yi_java3st_1team.ordermanagement.ui.panel.ORegisterPanel;
 import java.awt.Font;
 import java.awt.Color;
 
 @SuppressWarnings("serial")
-public class ClientOContent extends JPanel {
+public class ClientOMainPanel extends JPanel {
 	private JTabbedPane tpClientManagement;
 	private JPanel pORegister;
 	private JPanel pListPanel;
 	private ClientORegisterPanel pCORegisterPanel;
 	private ClientOListPanel pCOListPanel;
+	private ClientOCheckPanel pOCheckPanel;
 
-	public ClientOContent() {
+	public ClientOMainPanel() {
 
 		initialize();
 	}
@@ -60,5 +61,8 @@ public class ClientOContent extends JPanel {
 		pListPanel.setBackground(SystemColor.inactiveCaptionBorder);
 		tpClientManagement.addTab("주문 현황", null, pListPanel, null);
 		pListPanel.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		pOCheckPanel = new ClientOCheckPanel();
+		pListPanel.add(pOCheckPanel);
 	}
 }
