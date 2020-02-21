819ea98a50e486d8e389a3f7c5b4585721e47c7c
jamesareumin <hothihi5@gmail.com>
2020-02-21 15:40:45 +0900

    Dao 작성

--- src/main/java/yi_java3st_1team/main/dao/EmployeeDao.java
+++ src/main/java/yi_java3st_1team/main/dao/EmployeeDao.java
@@ -1,5 +1,14 @@
 package yi_java3st_1team.main.dao;
 
-public interface EmployeeDao {
+import java.util.List;
 
+import yi_java3st_1team.main.dto.Employee;
+
+public interface EmployeeDao {
+	Employee selectEmployeeByNo(Employee emp);
+	List<Employee> selectEmployeeByAll();
+	
+	int insertEmployee(Employee emp);
+	int updateEmployee(Employee emp);
+	int deleteEmployee(Employee emp);
 }
