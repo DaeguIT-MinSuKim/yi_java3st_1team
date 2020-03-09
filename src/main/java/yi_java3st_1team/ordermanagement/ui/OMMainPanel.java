a9f4e19d246ddd6b04e296c67b0a5153c02d309e
jamesareumin <hothihi5@gmail.com>
2020-03-09 22:38:49 +0900

    메인버튼 기능 구현, 현황조회& 로그아웃 오류는 수정중

--- src/main/java/yi_java3st_1team/ordermanagement/ui/OMMainPanel.java
+++ src/main/java/yi_java3st_1team/ordermanagement/ui/OMMainPanel.java
@@ -1,5 +1,8 @@
 package yi_java3st_1team.ordermanagement.ui;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Rectangle;
 import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
@@ -13,6 +16,7 @@ import yi_java3st_1team.clientmanagement.ui.CMMainPanel;
 import yi_java3st_1team.main.LeftMainMenu;
 import yi_java3st_1team.main.dto.Employee;
 import yi_java3st_1team.main.ui.EmployeeMainUIPanel;
+import yi_java3st_1team.main.ui.content.LogoImg02Panel;
 import yi_java3st_1team.main.ui.content.login.EmployeeLoginPanel;
 import yi_java3st_1team.ordermanagement.ui.content.OMContent;
 import yi_java3st_1team.productmanagement.ui.PMMainPanel;
@@ -77,6 +81,58 @@ public class OMMainPanel extends JPanel  implements ActionListener{
 				      }
 
 		   }
+	   private void actionPerformedMainBtn(ActionEvent e) {
+		   removeAll();
+		   revalidate();
+		   repaint();
+		   
+		   emp = new EmployeeMainUIPanel();
+		   add(emp);
+		   
+		   emp.pStop.removeAll();
+		   
+		   pEmpLogin = new EmployeeLoginPanel();
+		   emp.pStop.add(pEmpLogin, BorderLayout.WEST);
+
+		   switch (manager) {
+			case 1:
+				pEmpLogin.manager.setText("[책임관리자 로그인]");
+				pEmpLogin.loginImg.setIcon(new ImageIcon("D:\\workspace\\workspace_gradle\\yi_java3st_1team\\images\\loginMain\\topManager.png"));
+
+				// 책임관리자(대표이사~과장) 로그인시 차트 패널 불러오기
+				// 1. 라인차트
+				emp.pStop.remove(emp.pImg01); // 제거
+				JPanel line = new JPanel();
+				line.setBackground(SystemColor.yellow);
+				emp.pStop.add(line, BorderLayout.CENTER);
+
+				// 2. 바차트
+				emp.pImg02.remove(emp.pLogo); // 제거
+				JPanel bar = new JPanel();
+				bar.setBackground(Color.red);
+				bar.setPreferredSize(new Dimension(350, 250));
+				emp.pImg02.add(bar, BorderLayout.NORTH);
+				break;
+			case 2:
+				pEmpLogin.manager.setText("[관리자 로그인]");
+				pEmpLogin.loginImg.setIcon(new ImageIcon("D:\\workspace\\workspace_gradle\\yi_java3st_1team\\images\\loginMain\\manager2.png"));
+			    LogoImg02Panel logo01 = new LogoImg02Panel();
+			    emp.pStop.add(logo01,BorderLayout.CENTER);
+				break;
+			
+			}
+			pEmpLogin.loginSuc.setText("<html>반갑습니다!<br><span style='color:blue'>"+emp.loginEmp.getEmpName()+" "+emp.loginEmp.getEmpTitle()+"</span>님이<br>로그인 하셨습니다.</html>");
+			String empNum = String.format("EE%04d", emp.loginEmp.getEmpNo());
+			pEmpLogin.empInfo.setText("<html>- 사원번호 : <span style='color:red'>"+empNum+"</span><br>- 부서명 : <span style='color:green'>"+emp.loginEmp.getdNo().getDeptName()+"</span></html>");
+
+
+			   emp.pStop.revalidate();
+			   emp.pStop.repaint();
+			   
+			   pEmpLogin.btnLogout.addActionListener(this);
+
+	   }	
+	   
 	   private void btnCMActionPerformed(ActionEvent e) {
 	      removeAll();
 	      revalidate();
@@ -134,16 +190,6 @@ public class OMMainPanel extends JPanel  implements ActionListener{
 
 
 	   
-	   private void actionPerformedMainBtn(ActionEvent e) {
-		   removeAll();
-		   revalidate();
-		   repaint();
-		   setBounds(new Rectangle(0, 0, 1544, 700));
-		   EmployeeMainUIPanel emp = new EmployeeMainUIPanel();
-		   add(emp);
-		   revalidate();
-		   repaint();
 
-	   }	
 
 }
