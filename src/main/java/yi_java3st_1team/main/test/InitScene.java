ca261c955ab24ec1e93550803fa45fec48766ed9
jamesareumin <hothihi5@gmail.com>
2020-03-10 13:00:49 +0900

    메인화면 차트 구현중

--- src/main/java/yi_java3st_1team/main/test/InitScene.java
+++
@@ -1,7 +0,0 @@
-package yi_java3st_1team.main.test;
-
-import javafx.scene.Scene;
-
-public interface InitScene {
-	Scene createScene();
-}
--- src/main/java/yi_java3st_1team/main/ui/content/chart/InitScene.java
+++ src/main/java/yi_java3st_1team/main/ui/content/chart/InitScene.java
@@ -3,5 +3,5 @@ package yi_java3st_1team.main.ui.content.chart;
 import javafx.scene.Scene;
 
 public interface InitScene {
-	Scene createScene(); 
+	Scene createScene();
 }
