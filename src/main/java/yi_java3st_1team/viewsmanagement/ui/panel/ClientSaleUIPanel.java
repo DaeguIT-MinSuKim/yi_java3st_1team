35f420595334bd193ea20a7b06b0d1e087ac88c2
ksj-alt <ham_a127@naver.com>
2020-03-14 18:05:17 +0900

    고객별 판매현황 조회 - 테이블 및 조회 기능 구현

--- src/main/java/yi_java3st_1team/viewsmanagement/ui/panel/ClientSaleUIPanel.java
+++ src/main/java/yi_java3st_1team/viewsmanagement/ui/panel/ClientSaleUIPanel.java
@@ -1,11 +1,13 @@
 package yi_java3st_1team.viewsmanagement.ui.panel;
 
-import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.GridLayout;
 import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
@@ -14,20 +16,31 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
-import yi_java3st_1team.viewsmanagement.ui.content.ClientSalePanel;
+
+import yi_java3st_1team.viewsmanagement.dto.ClientSale;
 import yi_java3st_1team.viewsmanagement.ui.content.TotalAmountPanel;
 import yi_java3st_1team.viewsmanagement.ui.list.ClientSaleTblPanel;
-import java.awt.Color;
+import yi_java3st_1team.viewsmanagement.ui.service.ClientSaleUIService;
 
 @SuppressWarnings("serial")
 public class ClientSaleUIPanel extends JPanel {
+	private ClientSaleUIService service;
 	private JPanel pTitle;
+	private JTextField textField;
+	private JLabel lblTitle;
+	private JPanel pTop;
+	private JPanel pSearch;
+	private JLabel lblCName;
+	private JButton btnSearch;
+	private JCheckBox cbTotal;
+	private JPanel pList;
+	private ClientSaleTblPanel pClientList;
+	private JPanel pAmount;
+	private TotalAmountPanel pTotalAmount;
 
-	/**
-	 * Create the panel.
-	 */
+	
 	public ClientSaleUIPanel() {
-
+		service = new ClientSaleUIService();
 		initialize();
 	}
 	private void initialize() {
@@ -41,49 +54,90 @@ public class ClientSaleUIPanel extends JPanel {
 		add(pTitle);
 		pTitle.setLayout(null);
 		
-		JLabel lblTitle = new JLabel("\uACE0\uAC1D\uBCC4 \uD310\uB9E4\uD604\uD669 \uC870\uD68C");
+		lblTitle = new JLabel("\uACE0\uAC1D\uBCC4 \uD310\uB9E4\uD604\uD669 \uC870\uD68C");
 		lblTitle.setForeground(Color.BLACK);
 		lblTitle.setBounds(0, 30, 1500, 42);
 		lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
 		lblTitle.setFont(new Font("휴먼둥근헤드라인", Font.PLAIN, 32));
 		pTitle.add(lblTitle);
 		
-		JPanel pTop = new JPanel();
+		pTop = new JPanel();
 		pTop.setBounds(0, 72, 1500, 828);
 		pTop.setBackground(SystemColor.inactiveCaption);
 		pTop.setBorder(new EmptyBorder(40, 100, 50, 100));
 		pTitle.add(pTop);
 		pTop.setLayout(null);
 		
-		JPanel pSearch = new JPanel();
+		pSearch = new JPanel();
 		pSearch.setBounds(100, 40, 1300, 40);
 		pSearch.setPreferredSize(new Dimension(1300, 40));
 		pSearch.setBackground(SystemColor.inactiveCaption);
 		pTop.add(pSearch);
-		pSearch.setLayout(null);
+		pSearch.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
 		
-		ClientSalePanel pClientSale = new ClientSalePanel();
-		pClientSale.setBounds(0, 0, 1300, 40);
-		pSearch.add(pClientSale);
+		lblCName = new JLabel("고 객 상 호 명 ");
+		lblCName.setPreferredSize(new Dimension(150, 30));
+		lblCName.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCName.setForeground(Color.BLACK);
+		lblCName.setFont(new Font("휴먼둥근헤드라인", Font.PLAIN, 18));
+		pSearch.add(lblCName);
 		
-		JPanel pList = new JPanel();
+		textField = new JTextField();
+		textField.setPreferredSize(new Dimension(130, 30));
+		textField.setFont(new Font("굴림", Font.PLAIN, 16));
+		textField.setColumns(15);
+		pSearch.add(textField);
+		
+		btnSearch = new JButton("조회");
+		btnSearch.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String CName = textField.getText();
+				ClientSale cs = new ClientSale(CName);
+				try {
+					pClientList.loadDate(service.showClientSaleListByCName(cs));
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+		btnSearch.setPreferredSize(new Dimension(70, 30));
+		btnSearch.setForeground(Color.WHITE);
+		btnSearch.setFont(new Font("맑은 고딕", Font.BOLD, 16));
+		btnSearch.setBackground(new Color(70, 130, 180));
+		pSearch.add(btnSearch);
+		
+		cbTotal = new JCheckBox(" 전체");
+		cbTotal.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				pClientList.loadDate(service.showClientSaleList());
+			}
+		});
+		cbTotal.setPreferredSize(new Dimension(80, 30));
+		cbTotal.setHorizontalAlignment(SwingConstants.CENTER);
+		cbTotal.setForeground(Color.BLACK);
+		cbTotal.setFont(new Font("휴먼둥근헤드라인", Font.PLAIN, 16));
+		cbTotal.setBackground(SystemColor.inactiveCaption);
+		pSearch.add(cbTotal);
+		
+		pList = new JPanel();
 		pList.setBounds(100, 80, 1300, 658);
 		pList.setBackground(SystemColor.inactiveCaptionBorder);
 		pTop.add(pList);
 		pList.setLayout(null);
 		
-		ClientSaleTblPanel pClientList = new ClientSaleTblPanel();
+		pClientList = new ClientSaleTblPanel();
 		pClientList.setBounds(0, 0, 1300, 658);
 		pClientList.setBackground(SystemColor.inactiveCaptionBorder);
+		pClientList.loadDate(service.showClientSaleList());
 		pList.add(pClientList);
 		
-		JPanel pAmount = new JPanel();
+		pAmount = new JPanel();
 		pAmount.setBounds(100, 738, 1300, 40);
 		pAmount.setBackground(SystemColor.inactiveCaption);
 		pTop.add(pAmount);
 		pAmount.setLayout(null);
 		
-		TotalAmountPanel pTotalAmount = new TotalAmountPanel();
+		pTotalAmount = new TotalAmountPanel();
 		pTotalAmount.setBounds(0, 0, 1300, 40);
 		pAmount.add(pTotalAmount);
 	}
