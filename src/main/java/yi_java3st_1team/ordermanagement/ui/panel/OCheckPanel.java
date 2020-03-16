36131ad127fa739fd1ab80a393a20d441175c181
jsonhyun <airplant@naver.com>
2020-03-16 13:51:36 +0900

    주문관리 확인/처리에서 주문수정 기능 구현완료

--- src/main/java/yi_java3st_1team/ordermanagement/ui/panel/OCheckPanel.java
+++ src/main/java/yi_java3st_1team/ordermanagement/ui/panel/OCheckPanel.java
@@ -16,6 +16,7 @@ import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
 import javax.swing.SwingConstants;
 
+import yi_java3st_1team.ordermanagement.dto.Order;
 import yi_java3st_1team.ordermanagement.ui.list.OCheckTblPanel;
 import yi_java3st_1team.ordermanagement.ui.service.OrderUIService;
 
@@ -26,7 +27,6 @@ public class OCheckPanel extends JPanel {
 	private JPanel pList;
 	public JButton btnGoMain;
 	private OCheckTblPanel pOCheckList;
-	
 	private OrderUIService odService;
 	
 	public OCheckPanel() {
@@ -79,7 +79,7 @@ public class OCheckPanel extends JPanel {
 	private JPopupMenu createPopupMenu() {
 		JPopupMenu popMenu = new JPopupMenu();
 		
-		JMenuItem updateItem = new JMenuItem("수정");
+		JMenuItem updateItem = new JMenuItem("주문수정");
 		updateItem.addActionListener(myPopupMenuListener);
 		popMenu.add(updateItem);
 		
@@ -92,25 +92,43 @@ public class OCheckPanel extends JPanel {
 	//팝업메뉴 기능
 	ActionListener myPopupMenuListener = new ActionListener() {
 		
+		private JButton btnModify;
+		private OrderModify om;
+		private JFrame modify;
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			if(e.getActionCommand().equals("수정")) {
-				JFrame modify = new JFrame();
+			if(e.getActionCommand().equals("주문수정")) {
+				modify = new JFrame();
 				modify.setTitle("주문 수정");
 				modify.setSize(500,500);
-				OrderModify om = new OrderModify();
-				
+				om = new OrderModify();
+				Order order = pOCheckList.getSelectedItem();
+				om.setItem(order);
 				modify.setLocationRelativeTo(null);
 				modify.setResizable(false);
 				modify.getContentPane().add(om);
+				
+				btnModify = new JButton("수 정");
+				btnModify.addActionListener(this);
+				btnModify.setForeground(Color.WHITE);
+				btnModify.setBackground(new Color(135, 206, 235));
+				btnModify.setFont(new Font("맑은 고딕", Font.PLAIN, 12));
+				om.panel.add(btnModify);
+				
 				modify.setVisible(true);
 			}
-			
-			
+			if (e.getSource() == btnModify) {
+				btnModifyActionPerformed(e);
+			}
+						
 			if(e.getActionCommand().equals("주문취소")) {
 			}			
-			
-			
+		}
+		protected void btnModifyActionPerformed(ActionEvent e) {
+			odService.modifyOrder(om.getItem());
+			pOCheckList.loadDateCheck(odService.showOrderList());
+			modify.dispose();
 		}
 	};
 }
