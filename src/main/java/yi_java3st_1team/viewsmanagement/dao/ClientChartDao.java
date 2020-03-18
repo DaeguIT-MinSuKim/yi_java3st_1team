70fa54abdf758b393b73a7d73534fde9ddad7f91
ksj-alt <ham_a127@naver.com>
2020-03-18 18:33:43 -0400

    chart test

---
+++ src/main/java/yi_java3st_1team/viewsmanagement/dao/ClientChartDao.java
@@ -0,0 +1,13 @@
+package yi_java3st_1team.viewsmanagement.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import yi_java3st_1team.viewsmanagement.dto.ClientChart;
+import yi_java3st_1team.viewsmanagement.dto.ClientSale;
+
+public interface ClientChartDao {
+	List<ClientChart> procedureClientChart(ClientChart cChart) throws SQLException;
+	
+	List<ClientChart> selectClientChartTest();
+}
